0fa8601d4003ce71524a6b088ae48545f6f6a500
forizs22 <80420572+forizs22@users.noreply.github.com>
2021-03-15 18:17:45 +0100

    Update en/checkout

--- resources/lang/en/checkout.php
+++ resources/lang/en/checkout.php
@@ -4,6 +4,7 @@ return [
     'lessons' => 'Lessons',
     'how_many_hours' => 'How many lessons would you like to buy?',
     'lesson' => 'Lesson|Lesson',
+    'free_lesson' => 'Free trial',
     'minute' => 'minute|minute',
     'payment' => 'Pay',
     'address_label' => 'Address',
@@ -17,9 +18,14 @@ return [
     'product_name' => 'Product name',
     'price' => 'Price',
     'fee' => 'Fee',
-    'total_price' => 'Total price',
+    'total_price' => 'Total',
     'pay_btn' => 'Pay',
-    'footer_info' => 'All purchases are in HUF. Foreign transaction fees might apply according to the policies of your bank. You will be able to schedule your lessons after completing payment. Lessons must be scheduled within six months of purchase date. Refunds are in Consteach store credit only. Consteach credit can be used exclusively on Consteach.com and it has no cash value.'
+    'free_lesson_pay_btn' => 'Book lesson',
+    'payment_info_label' => 'Payment details',
+    'bank_card_info_label' => 'Credit card details',
+    'footer_info' => 'All purchases are in HUF. Foreign transaction fees might apply, according to your bank\'s policies. You will be able to schedule your lessons after purchase. Lessons must be scheduled within six months of purchase date. Refunds are in Consteach store credit only. Consteach credit has no cash value.',
+    'transaction_success_notification' => 'Successful payment',
+    'transaction_fail_notification' => 'Payment failed',
 ];
 
 ?>
