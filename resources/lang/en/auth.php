7dc2ef46c0b9980cdcd2d6795e34a300090a1559
forizs22 <80420572+forizs22@users.noreply.github.com>
2021-03-15 18:11:01 +0100

    Update en/auth

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,14 +13,14 @@ return [
     |
     */
 
-    'failed' => 'The account cannot be found based on the credentials provided.',
+    'failed' => 'There\'s no account with the info you provided.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
     'login_with_facebook_btn' => 'Continue with Facebook',
     'login_with_google_btn' => 'Continue with Google',
 
-    'register_as_student' => 'Registrate as student',
-    'register_as_teacher' => 'Registrate as teacher',
+    'register_as_student' => 'Sign up as student',
+    'register_as_teacher' => 'Sign up as teacher',
 
     'login_social_or_email' => 'or',
 
@@ -28,22 +28,24 @@ return [
     'last_name_label' => 'Last name',
     'email_label' => 'Email',
     'password_label' => 'Password',
-    'password_confirmation_label' => 'Confirm password',
+    'password_confirmation_label' => 'Password again',
     'remember_label' => 'Remember me',
 
     'login_btn' => 'Log in',
     'register_btn' => 'Sign up',
     'lost_password_btn' => 'Forgotten password',
-    'create_new_account_btn' => 'Create new account',
+    'create_new_account_btn' => 'Sign up with new account',
     'already_registered_btn' => 'I have an account',
     'password_reset_link_btn' => 'Send password reset',
 
     'forgot_password_title' => 'Forgotten password?',
-    'forgot_password_subtitle' => 'No worries, please provide your email address and we send you a message to help you request a new password', 
+    'forgot_password_subtitle' => 'Please provide your email and we will send you a message to help you reset it',
 
     'current_password_label' => 'Current password',
     'new_password_label' => 'New password',
     'new_password_confirmation_label' => 'New password again',
 
     'password_update_save_btn' => 'Update password',
+    'password_update_success_notification' => 'Successful password update',
+    'password_update_fail_notification' => 'Password update failed',
 ];
