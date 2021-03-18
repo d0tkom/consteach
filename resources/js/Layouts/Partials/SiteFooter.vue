e7e2e064206694bf88d19ed60f9bd876ce0199f8
Pina$en <huszerldani@gmail.com>
2021-03-18 04:06:45 +0100

    Link fixek

--- resources/js/Layouts/Partials/SiteFooter.vue
+++ resources/js/Layouts/Partials/SiteFooter.vue
@@ -17,20 +17,18 @@
 						{{ trans.get('footer.faq') }}
 					</c-btn>
 				</inertia-link>
-				<inertia-link href="/#registration">
-					<c-btn
-						text
-					>
-						{{ trans.get('footer.registration') }}
-					</c-btn>
-				</inertia-link>
-				<inertia-link href="/teacher-landing">
-					<c-btn
-						text
-					>
-						{{ trans.get('footer.teacher_application') }}
-					</c-btn>
-				</inertia-link>
+				<c-btn
+					@click="$root.openRegistrationPopup()"
+					text
+				>
+					{{ trans.get('footer.registration') }}
+				</c-btn>
+				<c-btn
+					text
+					:navigate-to="'/#teach-with-us'"
+				>
+					{{ trans.get('footer.teacher_application') }}
+				</c-btn>
 			</div>
 			<div class="column">
 				<div class="title font-bold color-primary-dark">
@@ -62,21 +60,21 @@
 				<div class="title font-bold color-primary-dark">
 					{{ trans.get('footer.policies_title') }}
 				</div>
-				<inertia-link href="#">
+				<inertia-link href="/privacy-policy">
 					<c-btn
 						text
 					>
 						{{ trans.get('footer.privacy') }}
 					</c-btn>
 				</inertia-link>
-				<inertia-link href="#">
+				<inertia-link href="/terms-and-conditions">
 					<c-btn
 						text
 					>
 						{{ trans.get('footer.terms_of_use') }}
 					</c-btn>
 				</inertia-link>
-				<inertia-link href="#">
+				<inertia-link href="/cookie-policy">
 					<c-btn
 						text
 					>
@@ -88,12 +86,12 @@
 				<div class="title font-bold">
 					{{ trans.get('footer.contact_title') }}
 				</div>
-				<inertia-link class="social" :href="trans.get('footer.facebook_url')">
+				<a class="social" target="_blank" :href="trans.get('footer.facebook_url')">
 					<img src="/img/facebook_icon.svg" alt="Facebook icon">
-				</inertia-link>
-				<inertia-link class="social" :href="trans.get('footer.instagram_url')">
+				</a>
+				<a class="social" target="_blank" :href="trans.get('footer.instagram_url')">
 					<img src="/img/instagram_icon.svg" alt=")Instagram icon">
-				</inertia-link>
+				</a>
 			</div>
 		</div>
 	</footer>
