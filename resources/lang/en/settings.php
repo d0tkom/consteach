d9b4a27bd446b2e6fe012cf8e5bcbe5bcee6dcfa
forizs22 <80420572+forizs22@users.noreply.github.com>
2021-04-11 21:53:51 +0200

    Update settings.php

--- resources/lang/en/settings.php
+++ resources/lang/en/settings.php
@@ -12,7 +12,7 @@ return [
     'spoken_language' => 'Spoken Languages',
     'language_level' => 'Level',
     'country_label' => 'From',
-    'teaching_language' => 'Taught Languages',
+    'teaching_language' => 'I teach',
     'timezone' => 'Timezone',
     'currency' => 'Currency',
     'site_language' => 'Site Language',
@@ -32,19 +32,19 @@ return [
     'one_hour_net' => '1 hour - Net',
     'one_hour_gross' => '1 hour - Gross',
 
-    'bulk_prices' => 'Csomagkedvezményes óradíjaim',
+    'bulk_prices' => 'Package hourly rate',
 
     'five_hours_net' => '5 hours - Net',
     'five_hours_gross' => '5 hours - Gross',
 
-    'ten_hours_net' => '10 óra - nettó',
-    'ten_hours_gross' => '10 óra - bruttó',
+    'ten_hours_net' => '10 óra - Net',
+    'ten_hours_gross' => '10 óra - Gross',
 
-    'twenty_hours_net' => '20 óra - nettó',
-    'twenty_hours_gross' => '20 óra - bruttó',
+    'twenty_hours_net' => '20 óra - Net',
+    'twenty_hours_gross' => '20 óra - Gross',
 
-    'validating_alert_title' => 'Validálás folyamatban',
-    'validating_alert_description' => 'A validálásig a főbb funkciók nem elérhetőek.',
+    'validating_alert_title' => 'Validation in progress.',
+    'validating_alert_description' => 'Main teacher functions are unavailable until validation is done.',
 
     'updated_success_notification' => 'Updated successfully',
     'updated_fail_notification' => 'Update failed',
