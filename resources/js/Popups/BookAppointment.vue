313dff4cc4a5df1b29b8799e5d5ce9a06fd3509c
Pinasen <huszerldani@gmail.com>
2021-05-14 01:15:56 +0200

    a foglalás popupnál 17:00 helyett 05:00 van

--- resources/js/Popups/BookAppointment.vue
+++ resources/js/Popups/BookAppointment.vue
@@ -51,7 +51,7 @@ export default {
 			return this.$moment.utc(this.data.date_start).tz(this.timeZone).format('YYYY. MMMM DD.');
 		},
 		time() {
-			return this.$moment.utc(this.data.date_start).tz(this.timeZone).format('hh:mm');
+			return this.$moment.utc(this.data.date_start).tz(this.timeZone).format('HH:mm');
 		}
 	},
 }
