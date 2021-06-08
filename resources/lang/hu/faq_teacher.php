0e7f25ce861d1d82c1d57b5a71800294ddba011d
forizs22 <80420572+forizs22@users.noreply.github.com>
2021-06-08 09:02:42 +0200

    Update faq_teacher.php

--- resources/lang/hu/faq_teacher.php
+++ resources/lang/hu/faq_teacher.php
@@ -18,23 +18,23 @@ return [
     'faq_tab_teacher' => 'Gyakori kérdések tanároknak',
     'faq_tab_student' => 'Gyakori kérdések diákoknak',
 
-    'question_01_title' => 'Ingyenes az oldal használata?',
-    'question_01_description' => 'Óráimon pozitív, megerősítő, kötetlen légkör biztosítja a gyorsabb nyelvi fejlődést. A humort fontosnak tartom a magánéletben és a nyelvórán is, meggyőződésem, hogy eredményesebben lehessen, hiszen így lehet minden jó és jó és jó és jó és jó',
+    'question_01_title' => 'Hogyan működik a vásárlás?',
+    'question_01_description' => 'Órát/csomagot vásárolni a diákok a tanár profilján tudnak, 1, 5, 10 vagy 20 órás felosztásban.',
 
-    'question_02_title' => 'Biztos ingyenes az oldal használata?',
-    'question_02_description' => 'Óráimon pozitív, megerősítő, kötetlen légkör biztosítja a gyorsabb nyelvi fejlődést. A humort fontosnak tartom a magánéletben és a nyelvórán is, meggyőződésem, hogy eredményesebben lehessen, hiszen így lehet minden jó és jó és jó és jó és jó',
+    'question_02_title' => 'Hogyan működik a tanár kifizetése?',
+    'question_02_description' => 'A tanárokat a Stripe fizetési rendszerén keresztül fizetjük ki.',
 
-    'question_03_title' => 'Nem ingyenes az oldal használata?',
-    'question_03_description' => 'Óráimon pozitív, megerősítő, kötetlen légkör biztosítja a gyorsabb nyelvi fejlődést. A humort fontosnak tartom a magánéletben és a nyelvórán is, meggyőződésem, hogy eredményesebben lehessen, hiszen így lehet minden jó és jó és jó és jó és jó',
+    'question_03_title' => 'Mennyibe kerül a regisztráció a Consteach-re?',
+    'question_03_description' => 'A regisztráció és az oldal használata tanároknak teljesen ingyenes.',
 
-    'question_04_title' => 'Ingyenes az oldal használata?',
-    'question_04_description' => 'Óráimon pozitív, megerősítő, kötetlen légkör biztosítja a gyorsabb nyelvi fejlődést. A humort fontosnak tartom a magánéletben és a nyelvórán is, meggyőződésem, hogy eredményesebben lehessen, hiszen így lehet minden jó és jó és jó és jó és jó',
+    'question_04_title' => 'Hogyan tudok előrébb kerülni a tanárkeresőben?',
+    'question_04_description' => 'A tanárkereső több szempontot vesz figyelembe, mint például a népszerűség, órai részvét, értékelési átlag, így az kerül előrébb, aki magas minőségben végzi az oktatást.',
 
-    'question_05_title' => 'Ingyenes az oldal használata?',
-    'question_05_description' => 'Óráimon pozitív, megerősítő, kötetlen légkör biztosítja a gyorsabb nyelvi fejlődést. A humort fontosnak tartom a magánéletben és a nyelvórán is, meggyőződésem, hogy eredményesebben lehessen, hiszen így lehet minden jó és jó és jó és jó és jó',
+    'question_05_title' => 'Mit tegyek, ha egy diák a platformon kívűl szeretne tanulni?',
+    'question_05_description' => 'A Consteach által jelentkező diákokkal a platformon kívűli órák tartása tilos. A diáknak ilyenkor jelzi az oktató, hogy a platform díj az ő munkájukat is segíti, a platform díjnak köszönhetően kapnak a tanárok diákokat és a diákok pedig folyamatosan fejlődő szolgáltatást.',
 
-    'question_06_title' => 'Ingyenes az oldal használata?',
-    'question_06_description' => 'Óráimon pozitív, megerősítő, kötetlen légkör biztosítja a gyorsabb nyelvi fejlődést. A humort fontosnak tartom a magánéletben és a nyelvórán is, meggyőződésem, hogy eredményesebben lehessen, hiszen így lehet minden jó és jó és jó és jó és jó',
+    'question_06_title' => 'Hogyan vehetem ki a pénzem?',
+    'question_06_description' => 'Az oktató a megtartott órák utáni díjat az oktatói tárcájában láthatja, melyet a tanári profiljának felületén tud kiutalni magának.',
 
     'more_btn' => 'Tovább',
     'less_btn' => 'Röviden',
