3fc3337c7f1dabc1c48f3be06c3e7b65e25f4a15
forizs22 <80420572+forizs22@users.noreply.github.com>
2021-06-11 06:59:52 +0200

    Update passwords.php

--- resources/lang/hu/passwords.php
+++ resources/lang/hu/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
+    'reset' => 'Jelszó frissítve!',
     'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
+    'throttled' => 'Kérjük várj mielőtt újrapróbálkozol.',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'user' => "Nincs regisztrált felhasználó ezzel az email címmel.",
 
 ];
